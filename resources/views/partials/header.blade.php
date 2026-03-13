e1d953eaa7d894ef5feebc214ef586c767c55fae
ihsanalidev <ihsanaliwork@gmail.com>
2026-03-13 11:39:21 +0700

    update navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -45,137 +45,50 @@
                 <ul class="navigation">
                     <li class="has-dropdown">
                         <span>Products</span>
-                        <ul class="dropdown-menu">
-                            <li><a href="">HR Core</a></li>
-                            <li><a href="">Enterprise Payroll</a></li>
-                            <li><a href="">Training</a></li>
-                            <li><a href="">Recruitment</a></li>
+                        <ul class="dropdown">
+                            <li><a href="#">HR Core</a></li>
+                            <li><a href="#">Enterprise Payroll</a></li>
+                            <li><a href="#">Training</a></li>
                         </ul>
                     </li>
                     <li class="has-dropdown">
                         <span>Services</span>
-                        <div class="submenu mega-submenu"> 
-                            <div class="mega-menu-left">
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link active-link">Our Journey</a>
-                                </div>
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link">Whistleblowing <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Governance <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Career <i class="fas fa-external-link-alt"></i></a>
-                                </div>
-                            </div>
-                            <div class="mega-menu-right">
-                                <div class="right-header">Our Journey</div>
-                                <ul class="right-links">
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Vision & Mission</a></li>
-                                    <li><a href="#">Award & Certification</a></li>
-                                    <li><a href="#">Leadership</a></li>
-                                    <li><a href="#">Privacy Policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
+                        <ul class="dropdown">
+                            <li><a href="#">Consulting</a></li>
+                            <li><a href="#">Implementations & Customo</a></li>
+                        </ul>
                     </li>
                     <li class="has-dropdown">
                         <span>Resources</span>
-                        <div class="submenu mega-submenu"> 
-                            <div class="mega-menu-left">
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link active-link">Our Journey</a>
-                                </div>
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link">Whistleblowing <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Governance <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Career <i class="fas fa-external-link-alt"></i></a>
-                                </div>
-                            </div>
-                            <div class="mega-menu-right">
-                                <div class="right-header">Our Journey</div>
-                                <ul class="right-links">
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Vision & Mission</a></li>
-                                    <li><a href="#">Award & Certification</a></li>
-                                    <li><a href="#">Leadership</a></li>
-                                    <li><a href="#">Privacy Policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
+                        <ul class="dropdown">
+                            <li><a href="#">HR Core</a></li>
+                            <li><a href="#">Enterprise Payroll</a></li>
+                            <li><a href="#">Training</a></li>
+                        </ul>
                     </li>
                     <li class="has-dropdown">
                         <span>Customers</span>
-                        <div class="submenu mega-submenu"> 
-                            <div class="mega-menu-left">
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link active-link">Our Journey</a>
-                                </div>
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link">Whistleblowing <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Governance <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Career <i class="fas fa-external-link-alt"></i></a>
-                                </div>
-                            </div>
-                            <div class="mega-menu-right">
-                                <div class="right-header">Our Journey</div>
-                                <ul class="right-links">
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Vision & Mission</a></li>
-                                    <li><a href="#">Award & Certification</a></li>
-                                    <li><a href="#">Leadership</a></li>
-                                    <li><a href="#">Privacy Policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
+                        <ul class="dropdown">
+                            <li><a href="#">HR Core</a></li>
+                            <li><a href="#">Enterprise Payroll</a></li>
+                            <li><a href="#">Training</a></li>
+                        </ul>
                     </li>
                     <li class="has-dropdown">
                         <span>Company</span>
-                        <div class="submenu mega-submenu"> 
-                            <div class="mega-menu-left">
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link active-link">Our Journey</a>
-                                </div>
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link">Whistleblowing <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Governance <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Career <i class="fas fa-external-link-alt"></i></a>
-                                </div>
-                            </div>
-                            <div class="mega-menu-right">
-                                <div class="right-header">Our Journey</div>
-                                <ul class="right-links">
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Vision & Mission</a></li>
-                                    <li><a href="#">Award & Certification</a></li>
-                                    <li><a href="#">Leadership</a></li>
-                                    <li><a href="#">Privacy Policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
+                        <ul class="dropdown">
+                            <li><a href="#">HR Core</a></li>
+                            <li><a href="#">Enterprise Payroll</a></li>
+                            <li><a href="#">Training</a></li>
+                        </ul>
                     </li>
                     <li class="has-dropdown">
                         <span>Contact Us</span>
-                        <div class="submenu mega-submenu"> 
-                            <div class="mega-menu-left">
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link active-link">Our Journey</a>
-                                </div>
-                                <div class="mega-menu-group">
-                                    <a href="#" class="mega-menu-link">Whistleblowing <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Governance <i class="fas fa-external-link-alt"></i></a>
-                                    <a href="#" class="mega-menu-link">Career <i class="fas fa-external-link-alt"></i></a>
-                                </div>
-                            </div>
-                            <div class="mega-menu-right">
-                                <div class="right-header">Our Journey</div>
-                                <ul class="right-links">
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Vision & Mission</a></li>
-                                    <li><a href="#">Award & Certification</a></li>
-                                    <li><a href="#">Leadership</a></li>
-                                    <li><a href="#">Privacy Policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
+                        <ul class="dropdown">
+                            <li><a href="#">HR Core</a></li>
+                            <li><a href="#">Enterprise Payroll</a></li>
+                            <li><a href="#">Training</a></li>
+                        </ul>
                     </li>
                 </ul>
 
